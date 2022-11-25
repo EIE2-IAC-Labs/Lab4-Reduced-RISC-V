e1ad4073622d1d894a0032dc80ebb3be5aef4e06
Samuel Wang <samuelpswang@gmail.com>
2022-11-25 17:08:19 +0000

    fix: fix alu.sv
    - syntax errors (; vs , / indentation errors)
    - width errors
    - edit default behavior

--- src/alu.sv
+++ src/alu.sv
@@ -1,40 +1,35 @@
-module ALU (
-    input logic [2:0] ALUctrl,
-    input logic ALUsrc,
-    input logic [31:0] regOp2,
-    input logic [31:0] ImmOp,
-    input logic [31:0] ALUop1,
-    output logic [31:1] ALUout,
-    output logic EQ
+module alu (
+  input logic ALUsrc,
+  input logic [2:0] ALUctrl,
+  input logic [31:0] ImmOp,
+  input logic [31:0] ALUop1,
+  input logic [31:0] regOp2,
+  output logic EQ,
+  output logic [31:0] ALUout, // note: 31:1 -> 31:0
 );
 
-    logic [31:0] ALUop2,
+// note: use correct indentation
+logic [31:0] ALUop2;
 
-    always_comb
-        if (ALUsrc)
-            ALUop2 = ImmOp;
-        else
-            ALUop2 = regOp2;
+always_comb
+  if (ALUsrc) ALUop2 = ImmOp;
+  else ALUop2 = regOp2;
     
-    always_comb
-        if (ALUout == 0) EQ = 1;
-        else EQ = 0;
+// note: unify number format
+always_comb
+  if (ALUout == 0) EQ = 1'b1;
+  else EQ = 1'b0;
 
-    always_comb
-        case (ALUctrl)
-            3'b000: begin
-                ALUout = ALUop1 + ALUop2;
-            end
-            3'b001: begin
-                ALUout = ALUop1 - ALUop2;
-            end 
-            default: begin
-                assign ALUout = 32'b0;
-                if (ALUop1 == ALUop2)
-                    assign EQ = 1'b1;
-                else
-                    assign EQ = 1'b0;
-            end 
-        endcase
+always_comb
+  case (ALUctrl)
+    3'b000: 
+      ALUout = ALUop1 + ALUop2;
+    3'b001:
+      ALUout = ALUop1 - ALUop2;
+    default: begin
+      ALUout = 32'b0;
+      EQ = 1'b0; // note: set everything to 0 at default
+    end
+  endcase
 
 endmodule

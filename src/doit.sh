1539eb28d12c32e69dd3cc1e7dbf4fa8de2e56ec
BharathaanSukumaran <bharathaansukumaran@gmail.com>
2022-11-26 17:01:18 +0000

    fix: fix doit.sh to accomodate bash

--- src/doit.sh
+++ src/doit.sh
@@ -2,10 +2,14 @@
 
 # user vars
 OBJ_DIR=build
-if [[ $# -eq 0 ]] then DUT_NAME=RISC;
-else DUT_NAME=$1_top; fi
-if [[ $# -eq 0 ]] then TB_NAME=$DUT_NAME;
-else TB_NAME=testbench/$1; fi
+if [[ $# -eq 0 ]] 
+then DUT_NAME=RISC;
+else DUT_NAME=$1_top; 
+fi
+if [[ $# -eq 0 ]] 
+then TB_NAME=$DUT_NAME;
+else TB_NAME=testbench/$1; 
+fi
 
 # script vars
 ID=`tput sgr0``tput bold``tput setab 7``tput setaf 0`
